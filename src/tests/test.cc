9f65e0d3723755ea7273462ffdd642d459742805
warbirdo <nanowatcher65@gmail.com>
2023-02-18 17:40:01 +0300

    Added test case when matrices are equal.

--- src/tests/test.cc
+++ src/tests/test.cc
@@ -20,6 +20,27 @@ TEST(EqualMatrix, NotEqualMatrix) {
     EXPECT_EQ(test_matrix.EqMatrix(test_matrix2), false);
 }
 
+TEST(EqualMatrix, EqualMatrix) {
+    S21Matrix test_matrix(2, 3);
+    test_matrix(0, 0) = 1.0;
+    test_matrix(0, 1) = -1.0;
+    test_matrix(0, 2) = 1.0;
+
+    test_matrix(1, 0) = -38.0;
+    test_matrix(1, 1) = 41.0;
+    test_matrix(1, 2) = -34.0;
+
+    S21Matrix test_matrix2(2, 3);
+    test_matrix2(0, 0) = 1.0;
+    test_matrix2(0, 1) = -1.0;
+    test_matrix2(0, 2) = 1.0;
+
+    test_matrix2(1, 0) = -38.0;
+    test_matrix2(1, 1) = 41.0;
+    test_matrix2(1, 2) = -34.0;
+    EXPECT_EQ(test_matrix.EqMatrix(test_matrix2), true);
+}
+
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
