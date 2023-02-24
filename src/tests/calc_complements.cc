697aa6a417c3b0d790a611cdf1cc6e6063266ef3
warbirdo <nanowatcher65@gmail.com>
2023-02-24 19:28:45 +0300

    delete 1 case.

--- src/tests/calc_complements.cc
+++ src/tests/calc_complements.cc
@@ -6,10 +6,3 @@ TEST(CalcComplements, incorrectMatrix) {
     S21Matrix test_matrix(2,3);
     EXPECT_THROW(test_matrix.CalcComplements(),std::invalid_argument);
 }
-
-TEST(CalcComplements, Matrix1x1) {
-    S21Matrix test_matrix(1,1);
-    test_matrix(0,0) = 3;
-    test_matrix.CalcComplements();
-//    EXPECT_DOUBLE_EQ(test_matrix,0);
-}
